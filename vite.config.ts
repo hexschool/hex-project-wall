f10840c4a4402a2780f0d917b9e8df583cf20876
yinmin <alice0050722@gmail.com>
2024-12-02 17:11:43 +0800

    fix: 更新 baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/hex-project-wall/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
